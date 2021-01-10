1a3d980b0337f47c420329e61bc9d46f80ec66fa
= <shaines@twilio.com>
2021-01-10 15:21:54 -0800

    added etc hosts example and updates to the run script and introduction notebook for ch-03

--- ch-03/start/docker/run.sh
+++ ch-03/start/docker/run.sh
@@ -8,13 +8,36 @@ function sparkConf() {
   echo "applying spark.conf"
 }
 
+function sparkExists() {
+  if [ -z "$SPARK_HOME" ]; then
+    echo "Please follow the installation instructions for Spark from Chapter 2."
+    exit 1
+  else
+    echo "SPARK_HOME is set. location=${SPARK_HOME}"
+  fi
+}
+
+function createNetwork() {
+  cmd="docker network ls | grep ${DOCKER_NETWORK_NAME}"
+  eval $cmd
+  retVal=$?
+  if [ $retVal -ne 0]; then
+    docker network create -d bridge ${DOCKER_NETWORK_NAME}
+  else
+    echo "docker network already exists ${DOCKER_NETWORK_NAME}"
+  fi
+}
+
 function start() {
-  docker network create -d bridge ${DOCKER_NETWORK_NAME}
+  # there is an expectation that $SPARK_HOME and $JAVA_HOME are both available to the session
+  sparkExists
+  createNetwork
   docker-compose -f ${DOCKER_COMPOSE_FILE} up -d --remove-orphans zeppelin
+  echo "Zeppelin will be running on http://127.0.0.1:8080"
 }
 
 function stop() {
-  docker-compose -f ${DOCKER_COMPOSE_FILE} down zeppelin
+  docker-compose -f ${DOCKER_COMPOSE_FILE} down --remove-orphans
 }
 
 case "$1" in
